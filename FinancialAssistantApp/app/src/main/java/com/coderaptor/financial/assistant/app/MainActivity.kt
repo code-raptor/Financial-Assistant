10da844dd5e772ca3b4a69294dee0744f3cf60fd
belakdavid <47664367+belakdavid@users.noreply.github.com>
2019-03-08 13:42:02 +0100

    change package name to correct source tree

--- FinancialAssistantApp/app/src/main/java/com/coderaptor/financial/assistant/app/MainActivity.kt
+++ FinancialAssistantApp/app/src/main/java/com/coderaptor/financial/assistant/app/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.coderaptor.financial.assistant.financial.assistant.app
+package com.coderaptor.financial.assistant.app
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.coderaptor.financial.assistant.financial.assistant.app.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
