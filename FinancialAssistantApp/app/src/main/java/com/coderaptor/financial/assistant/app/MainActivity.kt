a629282a4f363b64ff7ee06d270c463bff7f16c0
belakdavid <47664367+belakdavid@users.noreply.github.com>
2019-03-13 15:10:58 +0100

    Set up main activity

--- FinancialAssistantApp/app/src/main/java/com/coderaptor/financial/assistant/app/MainActivity.kt
+++ FinancialAssistantApp/app/src/main/java/com/coderaptor/financial/assistant/app/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.coderaptor.financial.assistant.app
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import com.baoyz.swipemenulistview.SwipeMenu
+import com.baoyz.swipemenulistview.SwipeMenuCreator
+import com.baoyz.swipemenulistview.SwipeMenuItem
+import com.baoyz.swipemenulistview.SwipeMenuListView
+import com.github.clans.fab.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,25 +22,77 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener {
-            val i = Intent(this, SettingsActivity::class.java)
-            startActivity(i)
+        val firstFab: FloatingActionButton = findViewById(R.id.addNewButton)
+        firstFab.setOnClickListener {
+            Log.i("fab", "addnew clicked")
         }
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        val settings: ImageButton = findViewById(R.id.settings)
+        Log.i("click", "gomb")
+        settings.setOnClickListener {
+            Log.i("click", "setting")
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
         }
+
+
+        val listView : SwipeMenuListView = findViewById(R.id.listView)
+
+        val list : MutableList<String> = ArrayList()
+
+        list.add("asd")
+        list.add("fasd")
+        list.add("asdsd")
+        list.add("asddf")
+        list.add("asdwqe")
+
+        val adapter: ArrayAdapter<Any> = ArrayAdapter(this, android.R.layout.simple_list_item_1, list as List<Any>)
+        listView.adapter = adapter
+
+        val creator = SwipeMenuCreator { menu ->
+            val openItem = SwipeMenuItem(
+                applicationContext
+            )
+            openItem.background = ColorDrawable(
+                Color.rgb(
+                    0xC9,
+                    0xC9,
+                    0xCE
+                )
+            )
+            openItem.width = (90)
+            openItem.setIcon(android.R.drawable.ic_menu_edit)
+            openItem.titleSize = 18
+            openItem.titleColor = Color.WHITE
+            menu.addMenuItem(openItem)
+
+            val deleteItem = SwipeMenuItem(applicationContext)
+            deleteItem.background = ColorDrawable(
+                Color.rgb(
+                    0xF9,
+                    0x3F,
+                    0x25
+                )
+            )
+            deleteItem.width = (90)
+            deleteItem.setIcon(android.R.drawable.ic_delete)
+            menu.addMenuItem(deleteItem)
+        }
+        listView.setMenuCreator(creator)
+
+        listView.setOnMenuItemClickListener(object : SwipeMenuListView.OnMenuItemClickListener {
+            override fun onMenuItemClick(position: Int, menu: SwipeMenu, index: Int): Boolean {
+                when (index) {
+                    0 -> {
+                        Log.d("click", "onMenuItemClick: clicked item ->$index")
+                    }
+                    1 -> {
+                        Log.d("click", "onMenuItemClick: clicked item ->$index")
+                    }
+                }
+                // false : close the menu; true : not close the menu
+                return false
+            }
+        })
     }
 }
