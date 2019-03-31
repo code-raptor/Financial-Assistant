5683171d4a6ce5838bd6e70c4249489061619f73
belakdavid <47664367+belakdavid@users.noreply.github.com>
2019-03-31 22:32:09 +0200

    Merge Emi's Gui (AddNewDream)

---
+++ FinancialAssistantApp/app/src/main/java/com/coderaptor/financial/assistant/app/core/Dream.kt
@@ -0,0 +1,19 @@
+package com.coderaptor.financial.assistant.app.core
+
+import com.coderaptor.financial.assistant.app.data.DatabaseHandler
+
+data class Dream(val name: String, val amount: Int, val where: String) {
+    var id: Long = -1
+
+    constructor(id: Long = -1, name: String, amount: Int, where: String): this(name, amount, where) {
+        this.id = id
+    }
+
+    companion object {
+        val CREATE_TABLE_DREAM = "CREATE TABLE IF NOT EXISTS ${DatabaseHandler.TABLE_NAME_DREAM} " +
+                "(${DatabaseHandler.BASE_ID} INTEGER PRIMARY KEY, " +
+                "${DatabaseHandler.BASE_NAME} TEXT, " +
+                "${DatabaseHandler.BASE_AMOUNT} INTEGER, " +
+                "${DatabaseHandler.WHERE_DREAM} TEXT);"
+    }
+}
