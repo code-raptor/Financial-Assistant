c479cbb607095a24ea7f5a948891b9ca052e6ef2
belakdavid <47664367+belakdavid@users.noreply.github.com>
2019-03-31 22:33:13 +0200

    Change Const Name

--- FinancialAssistantApp/app/src/main/java/com/coderaptor/financial/assistant/app/core/Transaction.kt
+++ FinancialAssistantApp/app/src/main/java/com/coderaptor/financial/assistant/app/core/Transaction.kt
@@ -14,10 +14,10 @@ data class Transaction(val amount: Int, val date: String, val name: String, val 
 
     companion object {
         val CREATE_TABLE_TRANSACTION = "CREATE TABLE IF NOT EXISTS ${DatabaseHandler.TABLE_NAME_TRANSACTION} " +
-                "(${DatabaseHandler.ID_BASE} INTEGER PRIMARY KEY, " +
-                "${DatabaseHandler.AMOUNT_TRANSACTION} INTEGER, " +
+                "(${DatabaseHandler.BASE_ID} INTEGER PRIMARY KEY, " +
+                "${DatabaseHandler.BASE_AMOUNT} INTEGER, " +
                 "${DatabaseHandler.DATE_TRANSACTION} TEXT, " +
-                "${DatabaseHandler.NAME_BASE} TEXT, " +
+                "${DatabaseHandler.BASE_NAME} TEXT, " +
                 "${DatabaseHandler.FREQUENCY_TRANSACTION} TEXT)"
     }
 }
