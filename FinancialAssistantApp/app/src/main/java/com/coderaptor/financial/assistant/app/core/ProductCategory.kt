a031172e0129af951fb3c77715d5c7341e19b3d3
belakdavid <47664367+belakdavid@users.noreply.github.com>
2019-04-01 15:26:37 +0200

    Add ProductCategory Core Class

---
+++ FinancialAssistantApp/app/src/main/java/com/coderaptor/financial/assistant/app/core/ProductCategory.kt
@@ -0,0 +1,13 @@
+package com.coderaptor.financial.assistant.app.core
+
+data class ProductCategory(val name: String, val productPropertyId: Long, val productPropertyValue: String) {
+    var id: Long = -1
+
+    constructor(id: Long, name: String, productPropertyId: Long, productPropertyValue: String): this(name, productPropertyId, productPropertyValue) {
+        this.id = id
+    }
+
+    companion object {
+        val CREATE_TABLE_PRODUCT_CATEGORY = ""
+    }
+}
