737f4359331deccfe4db5af859f50e981ff8f2e4
belakdavid <47664367+belakdavid@users.noreply.github.com>
2019-04-01 15:50:04 +0200

    Add Receipt Core Class

---
+++ FinancialAssistantApp/app/src/main/java/com/coderaptor/financial/assistant/app/core/Receipt.kt
@@ -0,0 +1,19 @@
+package com.coderaptor.financial.assistant.app.core
+
+data class Receipt(val date: String, val amount: Int, val product: Product) {
+    var id: Long = -1
+    var products = ArrayList<Product>()
+
+    constructor(id: Long, date: String, amount: Int, product: Product): this(date, amount, product) {
+        this.id = id
+    }
+
+    constructor(id: Long, date: String, amount: Int, products: ArrayList<Product>): this(date, amount, products[0]) {
+        this.id = id
+        this.products = products
+    }
+
+    companion object {
+        val CREATE_TABLE_RECEIPT = ""
+    }
+}
