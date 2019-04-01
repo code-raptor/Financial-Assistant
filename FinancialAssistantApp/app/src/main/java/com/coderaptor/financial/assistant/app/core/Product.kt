9c27cb14c056e3fb22a0d02a0bf0bb00e1db47d9
belakdavid <47664367+belakdavid@users.noreply.github.com>
2019-04-01 15:50:30 +0200

    Change base constructors

--- FinancialAssistantApp/app/src/main/java/com/coderaptor/financial/assistant/app/core/Product.kt
+++ FinancialAssistantApp/app/src/main/java/com/coderaptor/financial/assistant/app/core/Product.kt
@@ -1,8 +1,9 @@
 package com.coderaptor.financial.assistant.app.core
 
-data class Product(val name: String, val productCategoryId: Long) {
+data class Product(val name: String, val unit: String, val quantity: Int, val unitPrice: Int, val categoryId: Long) {
     var id: Long = -1
-    constructor(id: Long, name: String, productCategoryId: Long): this(name, productCategoryId) {
+
+    constructor(id: Long, name: String, unit: String, quantity: Int, unitPrice: Int, categoryId: Long): this(name, unit, quantity, unitPrice, categoryId) {
         this.id = id
     }
 
