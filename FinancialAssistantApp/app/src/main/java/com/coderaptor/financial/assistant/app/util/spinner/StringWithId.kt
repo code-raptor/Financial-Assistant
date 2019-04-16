b3a4deea26a2de384f174e36625602a0a0a09863
belakdavid <47664367+belakdavid@users.noreply.github.com>
2019-04-16 22:15:24 +0200

    Setup field data and CatagoryField(Spinner) working from database

---
+++ FinancialAssistantApp/app/src/main/java/com/coderaptor/financial/assistant/app/util/spinner/StringWithId.kt
@@ -0,0 +1,7 @@
+package com.coderaptor.financial.assistant.app.util.spinner
+
+class StringWithId (val string: String, val id: Long) {
+    override fun toString(): String {
+        return string
+    }
+}
