c91a48aa9f051e455173163d0a3aaa4126e97c7e
belakdavid <47664367+belakdavid@users.noreply.github.com>
2019-04-28 16:11:56 +0200

    Refactoring RecyclerView

--- FinancialAssistantApp/app/src/main/java/com/coderaptor/financial/assistant/app/HistoryActivity.kt
+++ FinancialAssistantApp/app/src/main/java/com/coderaptor/financial/assistant/app/HistoryActivity.kt
@@ -3,16 +3,21 @@ package com.coderaptor.financial.assistant.app
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.coderaptor.financial.assistant.app.adapters.TransactionAndReceiptAdapter
+import com.afollestad.recyclical.datasource.DataSource
+import com.afollestad.recyclical.datasource.dataSourceOf
+import com.afollestad.recyclical.setup
+import com.afollestad.recyclical.swipe.SwipeLocation
+import com.afollestad.recyclical.swipe.withSwipeAction
+import com.afollestad.recyclical.withItem
+import com.coderaptor.financial.assistant.app.adapters.ReceiptViewHolder
+import com.coderaptor.financial.assistant.app.adapters.TransactionViewHolder
+import com.coderaptor.financial.assistant.app.core.Receipt
+import com.coderaptor.financial.assistant.app.core.Transaction
 import com.coderaptor.financial.assistant.app.data.DatabaseHandler
-import com.coderaptor.financial.assistant.app.gui.SwipeToDeleteCallback
+import com.coderaptor.financial.assistant.app.util.toast
 import kotlinx.android.synthetic.main.activity_history.*
 
-class HistoryActivity: AppCompatActivity() {
+class HistoryActivity : AppCompatActivity() {
     private val dbHandler = DatabaseHandler(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,8 +32,6 @@ class HistoryActivity: AppCompatActivity() {
 
         list.addAll(transactions)
         list.addAll(receipts)
-
-        setUpRecyclerView(list)
 
         back.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
@@ -50,29 +53,68 @@ class HistoryActivity: AppCompatActivity() {
             startActivity(intent)
         }
 
-        val swipeHandler = object : SwipeToDeleteCallback(this) {
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                val adapter = recyclerView.adapter as TransactionAndReceiptAdapter
-                adapter.remove(viewHolder.adapterPosition, dbHandler)
+        val dataSource: DataSource<Any> = dataSourceOf(list)
+
+        recyclerView_h.setup {
+
+            withSwipeAction(SwipeLocation.LEFT) {
+                icon(R.drawable.ic_delete_white_24dp)
+                text(R.string.delete)
+                color(R.color.delete)
+                callback { index, item ->
+                    toast("delete $index: ${item}")
+                    if (item is Transaction) {
+                        dbHandler.deleteByPosition(item.id, DatabaseHandler.TABLE_NAME_TRANSACTION)
+                    } else if (item is Receipt) {
+                        dbHandler.deleteByPosition(item.id, DatabaseHandler.TABLE_NAME_RECEIPT)
+                    }
+                    true
+                }
             }
 
-            override fun onMove(
-                recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder,
-                target: RecyclerView.ViewHolder
-            ): Boolean = false
+            withSwipeAction(SwipeLocation.RIGHT) {
+                icon(R.drawable.ic_edit_white_24dp)
+                text(R.string.edit)
+                color(R.color.edit)
+                callback { index, item ->
+                    toast("edit $index: ${item}")
+                    if (item is Transaction) {
+                        //edit layout
+                    } else if (item is Receipt) {
+                        //edit layout
+                    }
+                    false
+                }
+            }
+
+            withDataSource(dataSource)
+
+            withItem<Transaction>(R.layout.list_income) {
+                onBind(::TransactionViewHolder) { _, item ->
+                    // PersonViewHolder is `this` here
+                    name.text = item.name
+                    date.text = item.date
+                    amount.text = "${item.amount}"
+                }
+                onClick { index ->
+                    // item is a `val` in `this` here
+                    toast("Clicked $index: ${item.name}")
+                }
+            }
+
+            withItem<Receipt>(R.layout.list_receipt) {
+                onBind(::ReceiptViewHolder) { _, item ->
+                    // PersonViewHolder is `this` here
+                    name.text = getString(R.string.receipt_string)
+                    date.text = item.date
+                    amount.text = "${item.amount}"
+                }
+                onClick { index ->
+                    // item is a `val` in `this` here
+                    toast("Clicked $index: ${item}")
+                }
+            }
         }
-        val itemTouchHelper = ItemTouchHelper(swipeHandler)
-        itemTouchHelper.attachToRecyclerView(recyclerView)
     }
-
-    private fun setUpRecyclerView(allList: MutableList<Any>) {
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        val transactionAndReceiptAdapter = TransactionAndReceiptAdapter(allList as ArrayList<Any>)
-        recyclerView.hasFixedSize()
-        recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = transactionAndReceiptAdapter
-    }
-
 }
 
