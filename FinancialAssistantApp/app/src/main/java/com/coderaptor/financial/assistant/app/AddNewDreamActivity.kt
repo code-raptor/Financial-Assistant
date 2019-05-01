bed9a2bfbe4942aed2ccb8e56315a53fc4db6f67
belakdavid <47664367+belakdavid@users.noreply.github.com>
2019-05-01 11:13:15 +0200

    Add Emi Dream, Product

--- FinancialAssistantApp/app/src/main/java/com/coderaptor/financial/assistant/app/AddNewDreamActivity.kt
+++ FinancialAssistantApp/app/src/main/java/com/coderaptor/financial/assistant/app/AddNewDreamActivity.kt
@@ -2,6 +2,7 @@ package com.coderaptor.financial.assistant.app
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
@@ -9,8 +10,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.coderaptor.financial.assistant.app.core.Dream
 import com.coderaptor.financial.assistant.app.data.DatabaseHandler
 import com.coderaptor.financial.assistant.app.gui.DreamActivity
+import com.coderaptor.financial.assistant.app.util.toast
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_newdream.*
+import kotlinx.android.synthetic.main.content_newdream.*
 
 class AddNewDreamActivity : AppCompatActivity() {
 
@@ -30,22 +33,37 @@ class AddNewDreamActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace rögzit", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
 
-            val amountField: EditText = findViewById(R.id.amountField)
-            val nameField: EditText = findViewById(R.id.nameField)
-            val whereField: EditText = findViewById(R.id.whereField)
+            val result = fieldsEmpty(amountField.text,nameField.text,whereField.text)
 
-            val amount = amountField.text.toString().toInt()
-            val name = nameField.text.toString()
-            val where = whereField.text.toString()
+            if(result) {
+                val amountField: EditText = findViewById(R.id.amountField)
+                val nameField: EditText = findViewById(R.id.nameField)
+                val whereField: EditText = findViewById(R.id.whereField)
 
-            val dream = Dream(name, amount, where)
+                val amount = amountField.text.toString().toInt()
+                val name = nameField.text.toString()
+                val where = whereField.text.toString()
 
-            dbHandler.insert(dream)
+                val dream = Dream(name, amount, where)
 
-            Toast.makeText(this, "Sikeres hozzáadás", Toast.LENGTH_LONG).show()
-            val intent = Intent(this, DreamActivity::class.java)
-            startActivity(intent)
+                dbHandler.insert(dream)
 
+                Toast.makeText(this, "Sikeres hozzáadás", Toast.LENGTH_LONG).show()
+                val intent = Intent(this, DreamActivity::class.java)
+                startActivity(intent)
+            }
+            else{
+                toast("Hiányzó adat!")
+            }
         }
+    }
+
+    fun fieldsEmpty(vararg fields: Editable):Boolean{
+        for (data in fields){
+            if(data.isEmpty()){
+                return false
+            }
+        }
+        return true
     }
 }
